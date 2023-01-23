99922cc50fb590e5de7f2bb0479e53ca9fcb2b55
Aivaras Jurgutis <you@example.com>
2023-01-23 23:04:25 +0200

    Projektasx, pirmas comit'as.

--- projecto_failai/public_html/index.php
+++ projecto_failai/public_html/index.php
@@ -1,21 +1,48 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+use Projektasx\Authenticator;
+use Projektasx\Controllers\AdminController;
+use Projektasx\Controllers\KontaktaiController;
+use Projektasx\Controllers\PersonController;
+use Projektasx\Controllers\PradziaController;
+use Projektasx\ExceptionHandler;
+use Projektasx\Output;
+use Projektasx\Router;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
-use KCS\SakykLabas;
-use KCS\DbConnect as DB;
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/larapack/dd/src/helper.php';
 
-echo "<meta charset='utf-8'>";
-SakykLabas::vardas('Vardenis');
+$log = new Logger('Portfolios');
+$log->pushHandler(new StreamHandler('../logs/klaidos.log', Logger::INFO));
 
-$host = 'db';
-$user = 'devuser';
-$password = 'devpass';
-$db = 'kcs_db';
+$output = new Output();
 
-DB::tikrintiPrisijungima($host, $user, $password, $db);
+try {
+    session_start();
 
-if(!empty($_REQUEST)){
-    echo '<hr>Gauti užklausos duomenys:<br><br>';
-    var_dump($_REQUEST);
+    $authenticator = new Authenticator();
+    $adminController = new AdminController($authenticator);
+    $kontaktaiController = new KontaktaiController($log);
+    $personController = new PersonController();
+
+    $router = new Router($output);
+    $router->addRoute('GET', '', [new PradziaController(), 'index']);
+    $router->addRoute('GET', 'admin', [$adminController, 'index']);
+    $router->addRoute('POST', 'login', [$adminController, 'login']);
+    $router->addRoute('GET', 'logout', [$adminController, 'logout']);
+    $router->addRoute('GET', 'kontaktai', [$kontaktaiController, 'index']);
+    $router->addRoute('GET', 'persons', [$personController, 'list']);
+    $router->addRoute('GET', 'person/new', [$personController, 'new']);
+    $router->addRoute('GET', 'person/delete', [$personController, 'delete']);
+    $router->addRoute('GET', 'person/edit', [$personController, 'edit']);
+    $router->addRoute('GET', 'person/show', [$personController, 'show']);
+    $router->addRoute('POST', 'person', [$personController, 'store']);
+    $router->addRoute('POST', 'person/update', [$personController, 'update']);
+    $router->run();
+}
+catch (Exception $e) {
+    $handler = new ExceptionHandler($output, $log);
+    $handler->handle($e);
 }
